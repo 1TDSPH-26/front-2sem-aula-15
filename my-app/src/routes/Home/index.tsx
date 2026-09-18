f4d273728284f4b862f8b872895ac92773006f95
isabelleferreiraa <isabelleferreiraa999@gmail.com>
2026-09-18 09:08:50 -0300

    feat: criando link dinamico com o perfil dos usuários

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -36,28 +36,28 @@ export default function Home() {
 
   const [usuarios, setUsuarios] = useState<TipoUsuarioGit[]>([]);
 
- useEffect(()=>{
+  useEffect(() => {
 
-  async function loadingData() {
-    try{
-      const response = await fetch(" https://api.github.com/users");
-      
-      if(!response.ok){
+    async function loadingData() {
+      try {
+        const response = await fetch(" https://api.github.com/users");
+
+        if (!response.ok) {
           throw new Error("A listagem dos usuários falhou!");
         }
 
-        const data:TipoUsuarioGit[] = await response.json();
+        const data: TipoUsuarioGit[] = await response.json();
 
         setUsuarios(data);
 
-    }catch(error){
-      console.log(error);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
 
-  // loadingData();
+    // loadingData();
 
- },[]);
+  }, []);
 
 
   return (
@@ -71,8 +71,12 @@ export default function Home() {
       </div>
       <div>
         <ul>
-          {usuarios.map( (u,indice)=>(
-            <li key={indice}>{u.id} - {u.login}</li>
+          {usuarios.map((u, indice) => (
+            <li key={indice}>{u.id} - {u.login} -
+
+              <a href={u.html_url} target="_blanck"><img src={u.avatar_url} alt={u.login} width={40} /></a>
+
+            </li>
           ))}
         </ul>
       </div>
