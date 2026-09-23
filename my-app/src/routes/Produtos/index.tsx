5f8d347e71e752fb481cd3075ab7597658078896
I.neeto99 <italo.neeto.oliv@outlook.com>
2026-09-23 09:18:02 -0300

    feat:Pré listagem da lista de produtos ok

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
-import type { TipoProduto } from "../../types/types";
+import { useEffect, useState } from "react";
+import type { TipoProduto } from "../types/types";
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  const [produtos, setProdutos] = useState<TipoProduto[]>([]);
+  const[produtos,setProdutos] = useState<TipoProduto[]>([]);
+
+  useEffect( ()=>{
+
+    const carregaProdutos = async ()=>{
+
+      try {
+        const resposta = await fetch("http://localhost:3001/produtos");
+        
+        if(!resposta.ok){
+          throw new Error("A listagem dos produtos falhou!");
+        }
+        const dados: TipoProduto[]= await resposta.json();
+        console.log(dados);
+
+      } catch (error) {
+        console.error(error);
+      }
+      
+
+    }
+
+    carregaProdutos();
+    
+  },[]);
 
   return (
     <main>
